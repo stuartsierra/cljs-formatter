d44845a49a06be31a4bfa9a85bbf2944b599452d
Stuart Sierra <mail@stuartsierra.com>
2012-06-06 21:08:46 -0400

    Update to ClojureScript release 0.0-1236

--- sample-app/project.clj
+++ sample-app/project.clj
@@ -1,7 +1,7 @@
 (defproject cljs-browser "0.0.1-SNAPSHOT"
   :description ""
   :dependencies [[org.clojure/clojure "1.4.0-beta4"]
-                 [org.clojure/clojurescript "0.0-993"]
+                 [org.clojure/clojurescript "0.0-1236"]
                  [ring "1.0.0-RC1"]
                  [compojure "0.6.4"]
                  [enlive "1.0.0"]]
