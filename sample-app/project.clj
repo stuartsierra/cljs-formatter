f97850e0fa162578bb605458598dde4fbedd62f1
Stuart Sierra <mail@stuartsierra.com>
2012-06-09 10:20:02 -0400

    Use Leiningen 2.0.0 for sample app project.clj

--- sample-app/project.clj
+++ sample-app/project.clj
@@ -1,16 +1,17 @@
 (defproject cljs-browser "0.0.1-SNAPSHOT"
   :description ""
-  :dependencies [[org.clojure/clojure "1.4.0-beta4"]
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojurescript "0.0-1236"]
                  [ring "1.0.0-RC1"]
                  [compojure "0.6.4"]
                  [enlive "1.0.0"]]
-  :repl-init one.sample.repl
-  :source-path "src/app/clj"
-  :extra-classpath-dirs ["src/app/cljs"
-                         "src/app/cljs-macros"
-                         "src/lib/clj"
-                         "src/lib/cljs"
-                         "templates"
-                         "../src"
-                         "../domina/src/cljs"])
+  :repl-options {:init-ns one.sample.repl}
+  :source-paths ["src/app/clj"
+                 "src/app/cljs"
+                 "src/app/cljs-macros"
+                 "src/lib/clj"
+                 "src/lib/cljs"
+                 "templates"
+                 "../src"
+                 "../domina/src/cljs"])
