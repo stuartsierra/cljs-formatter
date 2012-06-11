046a453e79c8b74bb2b012f3c179479ae9ec94f6
Stuart Sierra <mail@stuartsierra.com>
2012-06-10 21:14:05 -0400

    First working JavaScript code to arrange log lines

--- logview/src/logview/main.cljs
+++ logview/src/logview/main.cljs
@@ -1,5 +1,14 @@
 (ns logview.main
-  (:require [cljs-formatter :as format]))
+  (:require [cljs-formatter :as format]
+            [cljs.reader :as reader]
+            [domina :as d]))
 
 (defn ^:export start []
-  (js/alert "Hello, World!"))
+  (let [container (d/by-id "main")]
+    (doseq [div (d/nodes (d/by-class "log"))]
+      (d/set-html! div (format/html
+                        (reader/read-string
+                         (d/text div))))
+      (let [expr (first (d/children div))]
+        (format/arrange! expr container)
+        (format/set-toggle-on-click! expr container)))))
